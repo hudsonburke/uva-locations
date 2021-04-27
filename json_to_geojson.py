6de87f5c55f33dff53079e1fecbefcea4e1b6303
Hudson Burke <hpb7kr@virginia.edu>
2021-04-27 15:21:40 -0400

    Scripts and JSONs

---
+++ json_to_geojson.py
@@ -0,0 +1,31 @@
+#! usr/bin/env python
+
+from sys import argv
+from os.path import exists
+import simplejson as json 
+
+script, in_file, out_file = argv
+
+data = json.load(open(in_file))
+
+geojson = {
+    "type": "FeatureCollection",
+    "features": [
+    {
+        "type": "Feature",
+        "geometry" : {
+            "type": "Point",
+            "coordinates": [d["Longitude"], d["Latitude"]],
+            },
+        "properties" : {
+            "name": d["Name"],
+            "address": d["Address"],
+            "category": d["Category"]
+        },
+     } for d in data]
+}
+
+output = open(out_file, 'w')
+json.dump(geojson, output)
+
+print(geojson)
---
+++ new_geocode_converter.py
@@ -0,0 +1,43 @@
+#! usr/bin/env python
+
+from sys import argv
+from os.path import exists
+import simplejson as json 
+import googlemaps
+import string
+
+script, in_file, out_file = argv
+
+gmaps = googlemaps.Client(key='AIzaSyClPQsYyGZqDSaaMSd0OkzO9riSui_h0ok')
+data = json.load(open(in_file))
+geojson = {
+    "type": "FeatureCollection",
+    "features": [
+    ]
+}
+for d in data:
+    if d["Address"] == "" or not d["Address"][0].isnumeric():
+        continue
+    elif d["Longitude"] == None or d["Latitude"] == None:
+        temp = gmaps.geocode(str(d["Address"]) + ' Charlottesville, VA')[0]['geometry']['location']
+        coordinates = [temp['lng'], temp['lat']]
+    else:
+        coordinates = [d["Longitude"], d["Latitude"]]
+    new_feature = {
+        "type": "Feature",
+        "geometry" : {
+            "type": "Point",
+            "coordinates": coordinates,
+            },
+        "properties" : {
+            "name": string.capwords(d["Name"]),
+            "address": d["Address"],
+            "category": d["Category"]
+        },
+     }
+    geojson["features"].append(new_feature)
+
+output = open(out_file, 'w')
+json.dump(geojson, output)
+
+# print(geojson)
